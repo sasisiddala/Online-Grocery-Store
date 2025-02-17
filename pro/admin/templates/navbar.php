2d33c1c43476b779195123678c61ed8bf33535c0
sasisiddala <sasisiddala@gmail.com>
2025-02-17 23:05:40 +0530

    Add files via upload

---
+++ pro/admin/templates/navbar.php
@@ -0,0 +1,32 @@
+ <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+  <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="http://localhost/itp/index1.php">GROCERY STORE</a>
+  
+  <ul class="navbar-nav px-3">
+    <li class="nav-item text-nowrap">
+    	<?php
+    		if (isset($_SESSION['admin_id'])) {
+    			?>
+    				<a class="nav-link" href="http://localhost/itp/index1.php">Sign out</a>
+    			<?php
+    		}else{
+    			$uriAr = explode("/", $_SERVER['REQUEST_URI']);
+    			$page = end($uriAr);
+    			if ($page === "login.php") {
+    				?>
+	    				<a class="nav-link" href="../admin/register.php">Register</a>
+	    			<?php
+    			}else{
+    				?>
+	    				<a class="nav-link" href="../admin/login.php">Login</a>
+	    			<?php
+    			}
+
+
+    			
+    		}
+
+    	?>
+      
+    </li>
+  </ul>
+</nav>
